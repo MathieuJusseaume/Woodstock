61455ed98386b0f0f7a58102cb70e25b901c2a42
Julien Willy <julien.willy@gmail.com>
2023-09-21 10:49:28 +0200

    [FIX] - migration roles

--- backend/database/migrations/2023_09_20_144101_create_roles_table.php
+++ backend/database/migrations/2023_09_20_144101_create_roles_table.php
@@ -13,10 +13,9 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $tablme->string('name'); 
+            $table->string('name');
             $table->timestamps();
 
-            
         });
     }
 
