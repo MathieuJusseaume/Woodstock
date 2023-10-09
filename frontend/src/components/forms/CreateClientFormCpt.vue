43254692c8030715cf56d7bd33ee38cc58c24963
Mathilde-J <72550752+Mathilde-J@users.noreply.github.com>
2023-10-09 12:42:17 +0200

    [WIP] - Set up create client form with inputs

--- frontend/src/components/forms/CreateClientFormCpt.vue
+++ frontend/src/components/forms/CreateClientFormCpt.vue
@@ -1,42 +1,159 @@
 <template>
-    <form @submit.prevent="" class="createclientform">
+    <form @submit.prevent="" class="blockcreateclient__form">
 
-        <form @submit.prevent="" class="blockcreateclient__form">
+        <!-- <div class="form__field">
+            <label for="clientNames">Nom et Prénom</label>
+            <input id="clientNames" class="form__field__input" placeholder="" autocomplete @change="onChangeFields"
+                type="text" :value="`${fieldsValues.lastName} ${fieldsValues.firstName}`">
+        </div> -->
 
-            <div class="blockcreateclient__form__field">
-                <label for="">Nom et Prénom</label>
-                <input id="" class="blockcreateclient__form__field" autocomplete @change.prevent="" type="text"
-                    :value="">
+        <div class="createclientform__field__names form__field__flex">
+            <div class="form__field">
+                <label for="clientFirstName">Prénom</label>
+                <input id="clientFirstName" class="form__field__input" placeholder="" autocomplete
+                    @change.prevent="onChangeFields" type="text" :value="fieldsValues.clientFirstName">
             </div>
 
-            <div class="blockcreateclient__form__field">
-                <label for=""></label>
-                <input id="" class="blockcreateclient__form__field" autocomplete @change.prevent="" type="text"
-                    :value="">
+            <div class="form__field">
+                <label for="clientLastName">Nom</label>
+                <input id="clientLastName" class="form__field__input" placeholder="" autocomplete
+                    @change.prevent="onChangeFields" type="text" :value="fieldsValues.clientLastName">
+            </div>
+        </div>
+
+        <div class="createclientform__field__phone__email form__field__flex">
+            <div class="form__field">
+                <label for="clientPhoneNumber">Téléphone</label>
+                <input id="clientPhoneNumber" placeholder="" class="form__field__input" autocomplete
+                    @change.prevent="onChangeFields" type="text" :value="fieldsValues.clientPhoneNumber">
             </div>
 
-            <div class="createclientform__field">
-                <button class="createclientform__field__submit" type="submit">Créer</button>
+            <div class="form__field">
+                <label for="clientEmail">Adresse email</label>
+                <input id="clientEmail" placeholder="" class="form__field__input" autocomplete
+                    @change.prevent="onChangeFields" type="text" :value="fieldsValues.clientEmail">
+            </div>
+        </div>
+
+        <div class="form__field__billing__adress">
+            <div class="form__field">
+                <label for="clientBillingAdress">Adresse de facturation</label>
+                <input id="clientBillingAdress" placeholder="" class="form__field__input" autocomplete
+                    @change.prevent="onChangeFields" type="text"
+                    :value="fieldsValues.clientBillingAdress">
             </div>
 
-        </form>
+            <div class="form__field__billing_zipcode__city form__field__flex">
+                <div class="form__field">
+                    <label for="clientBillingZipCode">Code postal</label>
+                    <input id="clientBillingZipCode" placeholder="" class="form__field__input" autocomplete
+                        @change.prevent="onChangeFields" type="text"
+                        :value="fieldsValues.clientBillingZipCode">
+                </div>
+
+                <div class="form__field">
+                    <label for="clientBillingCity">Ville</label>
+                    <input id="clientBillingCity" placeholder="" class="form__field__input" autocomplete
+                        @change.prevent="onChangeFields" type="text"
+                        :value="fieldsValues.clientBillingCity">
+                </div>
+            </div>
+
+        </div>
 
 
+        <div class="form__field__delivery__adress">
+            <div class="form__field">
+                <label for="clientDeliveryAdress">Adresse de livraison</label>
+                <input id="clientDeliveryAdress" placeholder="" class="form__field__input" autocomplete
+                    @change.prevent="onChangeFields" type="text"
+                    :value="fieldsValues.clientDeliveryAdress">
+            </div>
 
+            <div class="form__field__delivery__zipcode__city form__field__flex">
+                <div class="form__field">
+                    <label for="clientDeliveryZipCode">Code postal</label>
+                    <input id="clientDeliveryZipCode" placeholder="" class="form__field__input" autocomplete
+                        @change.prevent="onChangeFields" type="text"
+                        :value="fieldsValues.clientDeliveryZipCode">
+                </div>
+
+                <div class="form__field">
+                    <label for="clientDeliveryCity">Ville</label>
+                    <input id="clientDeliveryCity" placeholder="" class="form__field__input" autocomplete
+                        @change.prevent="onChangeFields" type="text"
+                        :value="fieldsValues.clientDeliveryCity">
+                </div>
+            </div>
+        </div>
+
+        <div class="form__field">
+            <button class="form__field__submit" type="submit">Créer</button>
+        </div>
 
     </form>
 </template>
 
 <script>
+import { useClientsStore } from "@/stores/clientsStore.js";
+
 export default {
     name: "CreateClientFormCpt",
     data() {
         return {
-
+            hello: "test"
         }
     },
     methods: {
+        onChangeFields(event) {
+            console.log(event.target.value, event.target.id);
+            const clientStore = useClientsStore();
+            clientStore.setNewClient(event.target.value, event.target.id);
+        },
+    },
+    computed: {
+        fieldsValues() {
+            const clientStore = useClientsStore();
+            return clientStore.getCreateClientForm;
+        },
     }
 
 };
 </script>
+
+<style>
+.blockcreateclient__form {
+    min-width: 20em;
+    max-width: 40em;
+    width: 50%;
+    margin: auto;
+}
+
+.form__field__flex {
+    display: flex;
+    align-items: center;
+    gap: 1.5em;
+}
+
+.blockcreateclient__form label {
+    margin-bottom: .3em;
+    text-align: left;
+}
+
+.blockcreateclient__form input {
+    padding: .2em .5em;
+}
+
+.form__button__submit button {
+    margin: 0 auto;
+}
+
+.form__field__billing__adress,
+.form__field__delivery__adress {
+    margin-top: 2em;
+}
+
+.form__field__billing__adress div,
+.form__field__delivery__adress div {
+    margin: .5rem 0;
+}</style>
--- frontend/src/stores/clientsStore.js
+++ frontend/src/stores/clientsStore.js
@@ -2,96 +2,129 @@ import { defineStore } from "pinia";
 import { useUtilsStore } from "./utilsStore";
 
 export const useClientsStore = defineStore("clients", {
-    state: () => ({
-        clients: [],
-        createClientForm: {
-            lastName: "",
-            firstName: "",
-            /* etc. */
-        },
-        updateClientForm: {
-            lastName: "",
-            firstName: "",
-            /* etc. */
-        },
-    }),
-    getters: {
-        getClients: (state) => {
-            return state.clients;
-        },
-        getCreateClientForm: (state) => {
-            return state.createClientForm;
-        },
-        getUpdateClientForm: (state) => {
-            return state.updateClientForm;
-        },
-    },
-    actions: {
-        async getClientsAction() {
-            const utilsStore = useUtilsStore();
-            try {
-                utilsStore.toggleIsLoadingValue();
-                await new Promise(resolve => setTimeout(resolve, 1000));
-                const data = [
-                    {
-                        id: 0,
-                        last_name: "ing",
-                        first_name: "string",
-                        delivery_adress: "string",
-                        delivery_zip_code: 0,
-                        delivery_city: "string",
-                        billing_adress: "string",
-                        billing_zip_code: 0,
-                        billing_city: "string",
-                        email: "string",
-                        phone: "string",
-                    },
-                    {
-                        id: 1,
-                        last_name: "string",
-                        first_name: "string",
-                        delivery_adress: "string",
-                        delivery_zip_code: 0,
-                        delivery_city: "string",
-                        billing_adress: "string",
-                        billing_zip_code: 0,
-                        billing_city: "string",
-                        email: "string",
-                        phone: "string",
-                    },
-                    {
-                        id: 2,
-                        last_name: "string",
-                        first_name: "string",
-                        delivery_adress: "string",
-                        delivery_zip_code: 0,
-                        delivery_city: "string",
-                        billing_adress: "string",
-                        billing_zip_code: 0,
-                        billing_city: "string",
-                        email: "string",
-                        phone: "string",
-                    },
-                ];
-                this.clients = data;
-            } catch (error) {
-                console.log(error);
-            } finally {
-                utilsStore.toggleIsLoadingValue();
-            }
-        },
-        async updateClientAction() {
+	state: () => ({
+		clients: [],
+		createClientForm: {
+			clientLastName: "",
+			clientFirstName: "",
+			clientEmail: "",
+			clientPhoneNumber: "",
+			clientBillingAdress: "",
+			clientBillingZipCode: "",
+			clientBillingCity: "",
+			clientDeliveryAdress: "",
+			clientDeliveryZipCode: "",
+			clientDeliveryCity: ""
+		},
+		updateClientForm: {
+			clientLastName: "",
+			clientFirstName: "",
+			clientEmail: "",
+			clientPhoneNumber: "",
+			clientBillingAdress: "",
+			clientBillingZipCode: "",
+			clientBillingCity: "",
+			clientDeliveryAdress: "",
+			clientDeliveryZipCode: "",
+			clientDeliveryCity: ""
 
-        },
-        async deleteClientAction() {
-
-        },
-        getClientById() {
-
-        },
-        //on utilisera cette méthode lors de l'update d'un nouveau clien
-        setClientsAfterUpdate() {
-
-        },
-    }
+		},
+	}),
+	getters: {
+		getClients: (state) => {
+			return state.clients;
+		},
+		getCreateClientForm: (state) => {
+			return state.createClientForm;
+		},
+		getUpdateClientForm: (state) => {
+			return state.updateClientForm;
+		},
+	},
+	actions: {
+		async getClientsAction() {
+			const utilsStore = useUtilsStore();
+			try {
+				utilsStore.toggleIsLoadingValue();
+				await new Promise(resolve => setTimeout(resolve, 1000));
+				const data = [
+					{
+						id: 0,
+						last_name: "ing",
+						first_name: "string",
+						delivery_adress: "string",
+						delivery_zip_code: 0,
+						delivery_city: "string",
+						billing_adress: "string",
+						billing_zip_code: 0,
+						billing_city: "string",
+						email: "string",
+						phone: "string",
+					},
+					{
+						id: 1,
+						last_name: "string",
+						first_name: "string",
+						delivery_adress: "string",
+						delivery_zip_code: 0,
+						delivery_city: "string",
+						billing_adress: "string",
+						billing_zip_code: 0,
+						billing_city: "string",
+						email: "string",
+						phone: "string",
+					},
+					{
+						id: 2,
+						last_name: "string",
+						first_name: "string",
+						delivery_adress: "string",
+						delivery_zip_code: 0,
+						delivery_city: "string",
+						billing_adress: "string",
+						billing_zip_code: 0,
+						billing_city: "string",
+						email: "string",
+						phone: "string",
+					},
+				];
+				this.clients = data;
+			} catch (error) {
+				console.log(error);
+			} finally {
+				utilsStore.toggleIsLoadingValue();
+			}
+		},
+		async updateClientAction() {
+		},
+		async deleteClientAction() {
+		},
+		getClientById(clientId) {
+			console.log(clientId);
+		},
+		//on utilisera cette méthode lors de l'update d'un nouveau client
+		setClientsAfterUpdate() {
+		},
+		setNewClient(value, field) {
+			this.createClientForm[field] = value;
+		},
+		resetform(formToReset) {
+			this[formToReset] = {
+				clientLastName: "",
+				clientFirstName: "",
+				clientEmail: "",
+				clientPhoneNumber: "",
+				clientBillingAdressInfos: {
+					clientBilingAdress: "",
+					clientBillingZipCode: "",
+					clientBillingCity: ""
+				},
+				clientDeliveryAdressInfos: {
+					clientDeliveryAdress: "",
+					clientDeliveryZipCode: "",
+					clientDeliveryCity: ""
+				}
+			}
+		}
+	}
 });
