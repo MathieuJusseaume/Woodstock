cf6fbe158c57b50df33fe92eafe9ca7d19f811c3
Adrian Larenaudie <adrian.larenaudie@gmail.com>
2023-10-10 07:05:30 +0000

    [UPDATE] cors config seeding migrate

--- frontend/src/stores/authenticationStore.js
+++ frontend/src/stores/authenticationStore.js
@@ -1,10 +1,11 @@
 import { defineStore } from "pinia";
 import { useUtilsStore } from "./utilsStore";
+import axios from "axios";
 
 export const useAuthenticationStore = defineStore("authentication", {
     state: () => ({
-        email: "",
-        password: "",
+        email: "littel.lexie@example.com",
+        password: "password",
     }),
     getters: {
         getEmailValue: (state) => {
@@ -30,6 +31,8 @@ export const useAuthenticationStore = defineStore("authentication", {
             try {
                 utilsStore.toggleIsLoadingValue();
                 await new Promise(resolve => setTimeout(resolve, 1000));
+                const response = await axios.post("http://192.168.1.15:8080/api/login", { email: this.email, password: this.password });
+                console.log(response);
             } catch (error) {
                 console.log(error);
             } finally {
